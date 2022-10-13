35c5ccd1b857784bfbc59dbc640e1eabbcbdda57
Yurii <steamyurilazenko@gmail.com>
2022-10-13 20:55:09 +0300

    laboratorna03.4

---
+++ lab03.4/lab03.4.cpp
@@ -0,0 +1,25 @@
+﻿// Lab_03_4.cpp 
+// Лазенко Юрій 
+// Лабораторна робота № 3.4 
+// Розгалуження, задане плоскою фігурою. 
+// Варіант 12 
+#include <iostream> 
+using namespace std;
+int main()
+{
+    double x; // вхідний аргумент 
+    double y; // вхідний параметр 
+    double R;
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R = "; cin >> R;
+
+
+    // розгалуження в повній формі 
+    if ((pow(x + R, 2) + pow(y - R, 2) >= pow(R, 2) && y >= 0) || (pow(x - R, 2) + pow(y + R, 2) >= pow(R, 2) && y <= 0))
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+    cin.get();
+    return 0;
+}
